2e53190fef21959b8e9013cb342a2d8f1df83dee
Larry Ling <larryling.main@gmail.com>
2026-08-04 16:12:12 -0500

    feat: implement relay functions

--- Assets/Scripts/NetworkDebugManager.cs
+++ Assets/Scripts/NetworkDebugManager.cs
@@ -13,26 +13,12 @@ public class NetworkDebugManager : MonoBehaviour
     private void OnGUI()
     {
         GUILayout.BeginArea(new Rect(10, 10, 300, 300));
-        if (!m_NetworkManager.IsClient && !m_NetworkManager.IsServer)
-        {
-            StartButtons();
-        }
-        else
+        if (m_NetworkManager.IsClient || m_NetworkManager.IsServer)
         {
             StatusLabels();
         }
 
         GUILayout.EndArea();
-    }
-
-    private void StartButtons()
-    {
-        if (GUILayout.Button("Host"))
-            m_NetworkManager.StartHost();
-        if (GUILayout.Button("Client"))
-            m_NetworkManager.StartClient();
-        if (GUILayout.Button("Server"))
-            m_NetworkManager.StartServer();
     }
 
     private void StatusLabels()
